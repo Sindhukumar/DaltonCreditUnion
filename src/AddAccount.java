752d879b23998310b0c47fde84ec6397a2a80b8e
aakshay29 <aakshay.29@github.com>
2016-10-20 09:34:01 -0400

    Aakshay commit siz

--- src/AddAccount.java
+++ src/AddAccount.java
@@ -8,8 +8,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import Utilities.HashPassword;
+import customtools.ManageAccount;
+import customtools.ManageUser;
 import model.Dcuaccount;
+import model.Dcuuser;
 
 /**
  * Servlet implementation class AddAccount
@@ -39,15 +44,53 @@ public class AddAccount extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Date accountdate = new Date();
-		int type = 1; //checking = 1; savings = 0
-		int status = 1; //close = 0, open = 1
-		int userId = 1;
+		String nextUrl;
+		int type = Integer.parseInt(request.getParameter("username")); //checking = 1; savings = 0
+		int status = Integer.parseInt(request.getParameter("username")); //close = 0, open = 1
+		int userId = Integer.parseInt(request.getParameter("username"));
 		
-		Dcuaccount account = new Dcuaccount();
-		account.setAccountdate(accountdate);
-		account.setStatus(status);
-		account.setType(type);
-		//account.setDcuuser(dcuuser);
+		HttpSession session = request.getSession();
+		if(session.getAttribute("user") != null){
+			Dcuuser dcuuser = (Dcuuser) session.getAttribute("user");
+			Dcuaccount account = new Dcuaccount();
+			account.setAccountdate(accountdate);
+			account.setStatus(status);
+			account.setType(type);
+			account.setDcuuser(dcuuser);
+			ManageAccount.add(account);
+			
+			nextUrl = "/Home.jsp";
+		}
+		else{
+			String email = request.getParameter("username");
+			String name = request.getParameter("username");
+			String password = request.getParameter("username");
+			String passwordhash = HashPassword.Hash(email,password);
+			String phone = request.getParameter("username");
+			
+			if(ManageUser.isValidUser(email, passwordhash) == null){
+				Dcuuser dcuuser = new Dcuuser();
+				dcuuser.setEmail(email);
+				dcuuser.setName(name);
+				dcuuser.setPasswordhash(passwordhash);
+				dcuuser.setPhone(phone);
+				ManageUser.add(dcuuser);
+				session.setAttribute("user", dcuuser);
+				
+				Dcuaccount account = new Dcuaccount();
+				account.setAccountdate(accountdate);
+				account.setStatus(status);
+				account.setType(type);
+				account.setDcuuser(dcuuser);
+				ManageAccount.add(account);
+				
+				nextUrl = "/Home.jsp";
+			}
+			else{
+				nextUrl = "/Login.jsp";
+			}
+		}
+		response.sendRedirect(request.getContextPath() + nextUrl);
 	}
 
 }
--- src/customtools/ManageAccount.java
+++ src/customtools/ManageAccount.java
@@ -1,5 +1,23 @@
 package customtools;
 
-public class ManageAccount {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
+import model.Dcuuser;
+
+public class ManageAccount {
+	public static void add(Dcuuser user) {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		try {
+			trans.begin();
+			em.persist(user);
+			trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		} finally {
+			em.close();
+		}
+	}
 }
